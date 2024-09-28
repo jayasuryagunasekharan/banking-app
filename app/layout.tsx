d9cd82368c3a57a7fb5f79d56b4e3c1d3ca04a18
ashwinigit123 <ashwinitrale8@gmail.com>
2024-09-28 18:12:49 -0500

    addedd appwrite creds and user database

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const ibmPlexSerif = IBM_Plex_Serif({
 
 export const metadata: Metadata = {
   title: "Horizon",
-  description: "Horizon is a modern banking platform for everyone.",
+  description: "Horizon is a modern banking platform for people.",
   icons: {
     icon: '/icons/logo.svg'
   }
